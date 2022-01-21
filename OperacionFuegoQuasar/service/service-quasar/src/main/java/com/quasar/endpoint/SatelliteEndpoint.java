38caae255e5b4ceecaf5408e53907f64d480c07e
Jhon Fredy Hernandez <jhernandez@sysman.com.co>
2022-01-20 22:01:14 -0500

    ajustes persistencia

--- OperacionFuegoQuasar/service/service-quasar/src/main/java/com/quasar/endpoint/SatelliteEndpoint.java
+++ OperacionFuegoQuasar/service/service-quasar/src/main/java/com/quasar/endpoint/SatelliteEndpoint.java
@@ -1,11 +1,18 @@
 package com.quasar.endpoint;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.OptimisticLockException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -36,13 +43,23 @@ public class SatelliteEndpoint {
 	@PersistenceContext(unitName = "Quasar-persistence-unit")
 	private EntityManager em;
 
+	@Resource
+	private UserTransaction ut;
+
 	@Inject
 	private GetSatellites getsatellites;
 
 	@POST
 	@Consumes("application/json")
-	public Response create(Satellite entity) {
-		em.persist(entity);
+	public Response create(Satellite entity) throws QuasarException {
+		try {
+			ut.begin();
+			em.persist(entity);
+			ut.commit();
+		} catch (NotSupportedException | SystemException | SecurityException | IllegalStateException | RollbackException
+				| HeuristicMixedException | HeuristicRollbackException e) {
+			throw new QuasarException("No fue posible realizar la creación del satelite");
+		}
 		return Response
 				.created(UriBuilder.fromResource(SatelliteEndpoint.class).path(String.valueOf(entity.getId())).build())
 				.build();
@@ -50,12 +67,19 @@ public class SatelliteEndpoint {
 
 	@DELETE
 	@Path("/{id:[0-9][0-9]*}")
-	public Response deleteById(@PathParam("id") Long id) {
+	public Response deleteById(@PathParam("id") Long id) throws QuasarException {
 		Satellite entity = em.find(Satellite.class, id);
 		if (entity == null) {
 			return Response.status(Status.NOT_FOUND).build();
 		}
-		em.remove(entity);
+		try {
+			ut.begin();
+			em.remove(em.contains(entity) ? entity : em.merge(entity));
+			ut.commit();
+		} catch (NotSupportedException | SystemException | SecurityException | IllegalStateException | RollbackException
+				| HeuristicMixedException | HeuristicRollbackException e) {
+			throw new QuasarException("No fue posible realizar la eliminación del satelite");
+		}
 		return Response.status(Status.OK).build();
 	}
 
@@ -99,7 +123,7 @@ public class SatelliteEndpoint {
 	@Path("/{id:[0-9][0-9]*}")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Response update(@PathParam("id") Long id, Satellite entity) {
+	public Response update(@PathParam("id") Long id, Satellite entity) throws QuasarException {
 		if (entity == null) {
 			return Response.status(Status.BAD_REQUEST).build();
 		}
@@ -114,9 +138,14 @@ public class SatelliteEndpoint {
 		}
 
 		try {
+			ut.begin();
 			entity = em.merge(entity);
+			ut.commit();
 		} catch (OptimisticLockException e) {
 			return Response.status(Response.Status.CONFLICT).entity(e.getEntity()).build();
+		} catch (SecurityException | IllegalStateException | RollbackException | HeuristicMixedException
+				| HeuristicRollbackException | SystemException | NotSupportedException e) {
+			throw new QuasarException("No fue posible realizar la actualización del satelite");
 		}
 
 		return Response.ok(entity).build();
